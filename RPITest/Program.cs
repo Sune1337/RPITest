8eaccbffa1b69a649b9e6607c4a307afcd614ec4
Kristoffer Lindahl <kristoffer.lindahl@gmail.com>
2022-06-19 21:23:29 +0200

    Raise process and thread priority.

--- RPITest/Program.cs
+++ RPITest/Program.cs
@@ -1,9 +1,19 @@
-﻿using RPITest.Client;
+﻿using System.Diagnostics;
+
+using RPITest.Client;
 using RPITest.Server;
 
 if (args.Length == 0)
 {
     return;
+}
+
+// Set process priority
+using (var process = Process.GetCurrentProcess())
+{
+    process.PriorityClass = ProcessPriorityClass.High;
+    process.ProcessorAffinity = new IntPtr(1);
+    Thread.CurrentThread.Priority = ThreadPriority.Highest;
 }
 
 switch (args[0])
